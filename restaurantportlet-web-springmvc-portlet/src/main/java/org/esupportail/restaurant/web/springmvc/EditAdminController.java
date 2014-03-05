ad24434645b8a205825ae5d316d05eb9f248e2a5
Germain <germain.souquet@univ-lr.fr>
2014-03-05 11:05:42 +0100

    Fix user & admin areas setting + little other fixes

--- restaurantportlet-web-springmvc-portlet/src/main/java/org/esupportail/restaurant/web/springmvc/EditAdminController.java
+++ restaurantportlet-web-springmvc-portlet/src/main/java/org/esupportail/restaurant/web/springmvc/EditAdminController.java
@@ -29,162 +29,173 @@ import org.springframework.web.portlet.ModelAndView;
 @Controller
 @RequestMapping("EDIT")
 public class EditAdminController extends AbstractExceptionController {
-
-	@Autowired
-	private Authenticator authenticator;
-	@Autowired
-	private DatabaseConnector dc;
-	@Autowired
-	private RestaurantFeed flux;
-
+    
+    @Autowired
+    private Authenticator authenticator;
+    @Autowired
+    private DatabaseConnector dc;
+    @Autowired
+    private RestaurantFeed flux;
 
     @RequestMapping(params = {"action=adminSettings"})
     public ModelAndView renderEditAdminView(RenderRequest request, RenderResponse response) throws Exception {
 
-    	ModelMap model = new ModelMap();
+        ModelMap model = new ModelMap();
 
-    	User user = authenticator.getUser();
+        User user = authenticator.getUser();
+        model.put("user", user);
+
+//	    	try {
+
+        /* Get all area in the current feed */
+
+        try {
+            Set<String> areaList = new HashSet<String>();
+            for (Restaurant r : flux.getFlux().getRestaurants()) {
+                areaList.add(r.getArea());
+            }
+            model.put("areaList", areaList);
+        } catch (Exception e) {
+            // Here we go if the URL isn't set. 
+        }
+
+        String urlflux = null;
+        String[] areanames = null;
+        ResultSet results = null;
+        try {
+            results = dc.executeQuery("SELECT URLFLUX, AREANAME FROM PATHFLUX");
+            results.next();
+        } catch (SQLException e) {
+            // URL isn't set yet... 
+        }
+
+        try {
+            areanames = results.getString("AREANAME").split(",");
+        } catch (Exception e) {
+            // URL may be set be default area is not
+        }
+        try {
+            urlflux = results.getString("URLFLUX");
+        } catch (Exception e) {
+            // same issue as before
+        }
 
 
-	    	model.put("user", user);
 
-	    	try {
+        model.put("urlfluxdb", urlflux);
+        model.put("defaultArea", areanames);
 
-                /* Get all area in the current feed */
-	    		Set<String> areaList = new HashSet<String>();
-	    		for (Restaurant r : flux.getFlux().getRestaurants()) {
-	    			areaList.add(r.getArea());
-	    		}
+//	    	} catch (NullPointerException e) { e.printStackTrace(); }
 
-                model.put("areaList", areaList);
-	    		
-	    		ResultSet results = dc.executeQuery("SELECT URLFLUX, AREANAME FROM PATHFLUX");
-	    		results.next();
-	    		String urlflux;
-	    		String[] areanames;
-	    		try {
-	    			areanames = results.getString("AREANAME").split(",");
-	    		} catch (SQLException e) {
-	    			areanames = null;
-	    		}
-	    		try {
-	    			urlflux = results.getString("URLFLUX");
-	    		} catch (SQLException e) {
-	    			urlflux = null;
-	    		}
+        /* Action urlFlux set urlError if form URL was not well-formed */
+        String hasError = request.getParameter("urlError");
+        if (hasError != null) {
+            model.put("urlError", hasError);
+        }
 
-	        	model.put("urlfluxdb", urlflux);
-	        	model.put("defaultArea", areanames);
+        /* Action setDefaultArea set urlError if form URL was not well-formed */
+        String zoneSubmit = request.getParameter("zoneSubmit");
+        if (zoneSubmit != null) {
+            model.put("zoneSubmit", zoneSubmit);
+        }
 
-	    	} catch (NullPointerException e) { /**/ }
-
-            /* Action urlFlux set urlError if form URL was not well-formed */
-	    	String hasError = request.getParameter("urlError");
-	    	if (hasError != null)
-	    		model.put("urlError", hasError);
-	    	
-            /* Action setDefaultArea set urlError if form URL was not well-formed */
-	    	String zoneSubmit = request.getParameter("zoneSubmit");
-	    	if (zoneSubmit != null)
-	    		model.put("zoneSubmit", zoneSubmit);
-	    	
-            /* From ForceFeedUpdate */
-	    	if (request.getParameter("update") != null) {
-		    	Boolean isUpdated = new Boolean(request.getParameter("update"));
-		    	if (isUpdated.booleanValue()) {
-		    		model.put("updateFeed", "The feed has been correctly updated");
-	    		} else {
-	        		model.put("updateFeed", "The feed is already up to date");
-	        	}
-	    	}
-    	return new ModelAndView("edit-admin", model);
+        /* From ForceFeedUpdate */
+        if (request.getParameter("update") != null) {
+            Boolean isUpdated = new Boolean(request.getParameter("update"));
+            if (isUpdated.booleanValue()) {
+                model.put("updateFeed", "The feed has been correctly updated");
+            } else {
+                model.put("updateFeed", "The feed is already up to date");
+            }
+        }
+        return new ModelAndView("editadmin", model);
     }
+
     @RequestMapping(params = {"action=adminStats"})
     public ModelAndView renderStatsAdminView(RenderRequest request, RenderResponse response) throws Exception {
-       
+
         ModelMap model = new ModelMap();
-        
+
         /* Favorite Restaurants stats */
 
         ResultSet results = dc.executeQuery("SELECT RESTAURANTID FROM FAVORITERESTAURANT");
-        
+
         HashMap<Integer, Integer> nbRestaurant = new HashMap<Integer, Integer>();
-        
-        while(results.next()) {
+
+        while (results.next()) {
             int currentValue = (nbRestaurant.get(results.getInt("RESTAURANTID")) == null ? 0 : nbRestaurant.get(results.getInt("RESTAURANTID")));
-            if(currentValue == 0) {
+            if (currentValue == 0) {
                 nbRestaurant.put(results.getInt("RESTAURANTID"), 1);
             } else {
                 nbRestaurant.put(results.getInt("RESTAURANTID"), ++currentValue);
             }
         }
-        
-        HashMap<Integer, String> restaurantsName = new HashMap<Integer,String>();
-        for(Restaurant r : flux.getFlux().getRestaurants()) {
+
+        HashMap<Integer, String> restaurantsName = new HashMap<Integer, String>();
+        for (Restaurant r : flux.getFlux().getRestaurants()) {
             restaurantsName.put(r.getId(), r.getTitle());
         }
 
         /* Nutrition preferences stats */
-        
+
         ResultSet resultsNutrit = dc.executeQuery("SELECT NUTRITIONCODE, COUNT(*) FROM NUTRITIONPREFERENCES GROUP BY NUTRITIONCODE");
         Map<Integer, Integer> prefCodeList = new HashMap<Integer, Integer>();
-        
-        while(resultsNutrit.next()) {
+
+        while (resultsNutrit.next()) {
             prefCodeList.put(resultsNutrit.getInt(1), resultsNutrit.getInt(2));
         }
-        
+
         model.put("prefCodeList", prefCodeList);
         model.put("stats", nbRestaurant);
         model.put("restaurantsName", restaurantsName);
-                     
+
         return new ModelAndView("stats", model);
-    }  
+    }
 
     @RequestMapping(params = {"action=urlFlux"})
-    public void setURLFlux(ActionRequest request, ActionResponse response, @RequestParam(value = "url", required = true) String url) throws Exception { 	
-    	response.setRenderParameter("action", "adminSettings");
-    	try {
-    	    URL urlFlux = new URL(url);	
-    		flux.setPath(urlFlux);
-    		response.setRenderParameter("urlError", "false");
-    		// If URL is correct, then we can insert this into the database. 
-			ResultSet results = dc.executeQuery("SELECT URLFLUX FROM PATHFLUX");
-			results.next();
-			results.updateString("urlflux", url);
-			results.updateRow();
-    	} catch (SQLException e) {
+    public void setURLFlux(ActionRequest request, ActionResponse response, @RequestParam(value = "url", required = true) String url) throws Exception {
+        response.setRenderParameter("action", "adminSettings");
+        try {
+            URL urlFlux = new URL(url);
+            flux.setPath(urlFlux);
+            response.setRenderParameter("urlError", "false");
+            // If URL is correct, then we can insert this into the database. 
+            ResultSet results = dc.executeQuery("SELECT * FROM PATHFLUX");
+            results.next();
+            results.updateString("URLFLUX", url);
+            results.updateRow();
+        } catch (SQLException e) {
             // We get a SQLException if the row doesn't exist in the table
-    		dc.executeUpdate("INSERT INTO PATHFLUX (URLFLUX) VALUES ('"+ url +"')");
-    	} catch (Exception e) {
-    		response.setRenderParameter("urlError", "true");
-    	}
+            dc.executeUpdate("INSERT INTO PATHFLUX (URLFLUX) VALUES ('" + url + "')");
+        } catch (Exception e) {
+            response.setRenderParameter("urlError", "true");
+        }
     }
-    
+
     @RequestMapping(params = {"action=setDefaultArea"})
     public void setDefaultArea(ActionRequest request, ActionResponse response, @RequestParam(value = "chkArea[]", required = false) String[] listAreas) throws Exception {
         String areanames = "";
-        if(listAreas != null) {
-            for(int i=0; i<listAreas.length; i++) {
-                areanames += listAreas[i] + (i<listAreas.length-1 ? "," : "");
+        if (listAreas != null) {
+            for (int i = 0; i < listAreas.length; i++) {
+                areanames += listAreas[i] + (i < listAreas.length - 1 ? "," : "");
             }
         }
-        
+
         response.setRenderParameter("action", "adminSettings");
         response.setRenderParameter("zoneSubmit", "true");
-        
-        ResultSet results = dc.executeQuery("SELECT AREANAME FROM PATHFLUX");
+
+        ResultSet results = dc.executeQuery("SELECT * FROM PATHFLUX");
         results.next();
         results.updateString("AREANAME", areanames);
         results.updateRow();
-    }    
-    
+    }
+
     @RequestMapping(params = {"action=forceFeedUpdate"})
     public void feedUpdate(ActionRequest request, ActionResponse response) throws Exception {
 
-		Boolean isUpdated = flux.update();
-        
-    	response.setRenderParameter("action", "adminSettings");
-    	response.setRenderParameter("update", isUpdated.toString());
-    }
+        Boolean isUpdated = flux.update();
 
+        response.setRenderParameter("action", "adminSettings");
+        response.setRenderParameter("update", isUpdated.toString());
+    }
 }
--- restaurantportlet-web-springmvc-portlet/src/main/java/org/esupportail/restaurant/web/springmvc/EditController.java
+++ restaurantportlet-web-springmvc-portlet/src/main/java/org/esupportail/restaurant/web/springmvc/EditController.java
@@ -30,162 +30,164 @@ import org.springframework.web.portlet.ModelAndView;
 @RequestMapping("EDIT")
 public class EditController extends AbstractExceptionController {
 
-	@Autowired
-	private Authenticator authenticator;
-	@Autowired
-	private DatabaseConnector dc;
-	@Autowired
-	private RestaurantFeed flux;
+    @Autowired
+    private Authenticator authenticator;
+    @Autowired
+    private DatabaseConnector dc;
+    @Autowired
+    private RestaurantFeed flux;
 
-	  @RequestMapping
-	    public ModelAndView renderEditView(RenderRequest request, RenderResponse response) throws Exception {
-	        
-	    	ModelMap model = new ModelMap();
+    @RequestMapping
+    public ModelAndView renderEditView(RenderRequest request, RenderResponse response) throws Exception {
 
-	    	User user = authenticator.getUser();
-	    	model.put("user", user);
+        ModelMap model = new ModelMap();
 
-	    	int[] code     = {1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15};
-			model.put("nutritionCodes", code);
+        User user = authenticator.getUser();
+        model.put("user", user);
 
-			try {
+        int[] code = {1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15};
+        model.put("nutritionCodes", code);
 
-				ResultSet prefUser = dc.executeQuery("SELECT NUTRITIONCODE FROM nutritionPreferences WHERE USERNAME='"+ user.getLogin() +"';");
+        try {
 
-				Set<String> nutritionPrefs = new HashSet<String>();
+            ResultSet prefUser = dc.executeQuery("SELECT NUTRITIONCODE FROM nutritionPreferences WHERE USERNAME='" + user.getLogin() + "';");
 
-				while (prefUser.next()) {
-					nutritionPrefs.add(prefUser.getString("NUTRITIONCODE"));
-				}
+            Set<String> nutritionPrefs = new HashSet<String>();
 
-				model.put("nutritionPrefs", nutritionPrefs);
+            while (prefUser.next()) {
+                nutritionPrefs.add(prefUser.getString("NUTRITIONCODE"));
+            }
 
-				prefUser.close();
-			} catch(SQLException e) { /**/ }
+            model.put("nutritionPrefs", nutritionPrefs);
 
-	    	try {
-	    	    Set<String> areaList = new HashSet<String>();
-                for (Restaurant r : flux.getFlux().getRestaurants()) {
-                    areaList.add(r.getArea());
-                }
+            prefUser.close();
+        } catch (SQLException e) { /**/ }
 
-                model.put("areaList", areaList);
+        try {
+            Set<String> areaList = new HashSet<String>();
+            for (Restaurant r : flux.getFlux().getRestaurants()) {
+                areaList.add(r.getArea());
+            }
 
-	    		String userArea[] = null;
-	    		try {
-	    			ResultSet results = dc.executeQuery("SELECT AREANAME FROM USERAREA WHERE USERNAME='"+user.getLogin()+"';");
-	    			results.next();
-	    			userArea = results.getString("AREANAME").split(",");
-	    			results.close();
-	    		} catch (SQLException e) {
-	    			// here we are if the user doesn't already have a specific area setting.
-	    			ResultSet results = dc.executeQuery("SELECT AREANAME FROM PATHFLUX");
-	    			results.next();
-	    			try {
-	    				userArea = results.getString("AREANAME").split(",");
-	    			} catch (SQLException e2) {
-	    				// No default area for all user, admin must configure the portlet. 
-	    				// No need to throw an exception
-	    			}
-	    			results.close();
+            model.put("areaList", areaList);
 
-	    		}
-	    		model.put("defaultArea", userArea);
-
-	        	Set<String> favResults = new HashSet<String>();
-	        	try {
-	        		ResultSet results = dc.executeQuery("SELECT RESTAURANTID FROM FAVORITERESTAURANT WHERE USERNAME='"+ user.getLogin() +"'");
-	        		while (results.next()) {
-	        			favResults.add(results.getString("restaurantId"));
-	        		}
-	        		results.close();
-	        	} catch (Exception e) {
-	        		// No data available, doesn't matter 
-	        	}
-	        	model.put("favList", favResults);
-
-	        	List<Restaurant> listRestaurant = flux.getFlux().getRestaurants();
-	        	List<Restaurant> listFavRestaurant = new ArrayList<Restaurant>();
-
-	        	for (Restaurant r : listRestaurant) {
-	        		for (String favId : favResults) {
-	        			if (r.getId() == Integer.parseInt(favId, 10)) {
-	        			    listFavRestaurant.add(r);
-	        			}
-	        		}
-	        	}
-
-	        	model.put("listFavRestaurant", listFavRestaurant);
-
-	    	} catch (NullPointerException e) { /**/ }
-
-	    	/* param from setUserArea action */
-	    	String zoneSubmit = request.getParameter("zoneSubmit");
-	    	if (zoneSubmit != null) {
-	    		model.put("zoneSubmit", zoneSubmit);
-	    	}
-	    	/* param from nutritionPreferences action */
-	    	String nutritSubmit = request.getParameter("nutritSubmit");
-	    	if (nutritSubmit != null) {
-	    		model.put("nutritSubmit", nutritSubmit);
-	    	}
-	    	return new ModelAndView("edit", model);
-	    }
-
-	    @RequestMapping(value = {"EDIT"}, params = {"action=nutritionPreferences"})
-	    public void setUserNutritionPreferences(ActionRequest request, ActionResponse response) throws Exception {
-
-	    	String userLogin = authenticator.getUser().getLogin();
-
-	    	Map parameters = request.getParameterMap();
-	    	  	
-	    	
-	    	int[] code     = {1,2,3,4,5,6,7,9,10,11,12,13,14,15};
-
-	    	for(int i=0; i<code.length; i++) {
-
-	    		if (parameters.get("code-" + code[i]) != null) {
-
-	    			try {
-	    				dc.executeUpdate("INSERT INTO nutritionPreferences (USERNAME, NUTRITIONCODE) VALUES ('"+ userLogin +"', '"+ code[i] +"');");
-	    			} catch (SQLException e) { /**/ }
-
-	    		} else {
-	    			/* uncheck boxes need to be deleted from the db
-	    			   if it goes into the catch it mean the row didn't exist and it doesn't need specific treatment */
-	    			try {
-	    				dc.executeUpdate("DELETE FROM nutritionPreferences WHERE USERNAME='"+ userLogin +"' AND  NUTRITIONCODE='"+ code[i] +"';");
-	    			} catch (SQLException e) { /**/ }
-
-	    		}
-	    	}
-
-	    	response.setRenderParameter("nutritSubmit", "true");
-	    }
-
-	    @RequestMapping(params = {"action=setUserArea"})
-	    public void setUserArea(ActionRequest request, ActionResponse response, @RequestParam(value = "chkArea[]", required = false) String[] listAreas) throws Exception {
-	        
-            User user = authenticator.getUser();
-	        
-            String areanames = "";
-	        
-            if(listAreas != null) {
-	            for(int i=0; i<listAreas.length; i++) {
-	                areanames += listAreas[i] + (i<listAreas.length-1 ? "," : "");
-	            }
-	        }
-	        
-	        try {
-    	        ResultSet results = dc.executeQuery("SELECT AREANAME FROM USERAREA WHERE USERNAME='" + user.getLogin() + "';");
+            String userArea[] = null;
+            try {
+                ResultSet results = dc.executeQuery("SELECT AREANAME FROM USERAREA WHERE USERNAME='" + user.getLogin() + "';");
                 results.next();
-    	        results.updateString("AREANAME", areanames);
-    	        results.updateRow();
+                userArea = results.getString("AREANAME").split(",");
                 results.close();
-    	    } catch (SQLException e) {
-                dc.executeUpdate("INSERT INTO USERAREA (USERNAME, AREANAME) VALUES ('"+user.getLogin()+"', '"+areanames+"');");
-    	    }
+            } catch (SQLException e) {
+                // here we are if the user doesn't already have a specific area setting.
+                ResultSet results = dc.executeQuery("SELECT AREANAME FROM PATHFLUX");
+                results.next();
+                try {
+                    userArea = results.getString("AREANAME").split(",");
+                } catch (SQLException e2) {
+                    // No default area for all user, admin must configure the portlet. 
+                    // No need to throw an exception
+                }
+                results.close();
 
-	    	response.setRenderParameter("zoneSubmit", "true");
-	    }
+            }
+            model.put("defaultArea", userArea);
+
+            Set<String> favResults = new HashSet<String>();
+            try {
+                ResultSet results = dc.executeQuery("SELECT RESTAURANTID FROM FAVORITERESTAURANT WHERE USERNAME='" + user.getLogin() + "'");
+                while (results.next()) {
+                    favResults.add(results.getString("restaurantId"));
+                }
+                results.close();
+            } catch (Exception e) {
+                // No data available, doesn't matter 
+            }
+            model.put("favList", favResults);
+
+            List<Restaurant> listRestaurant = flux.getFlux().getRestaurants();
+            List<Restaurant> listFavRestaurant = new ArrayList<Restaurant>();
+
+            for (Restaurant r : listRestaurant) {
+                for (String favId : favResults) {
+                    if (r.getId() == Integer.parseInt(favId, 10)) {
+                        listFavRestaurant.add(r);
+                    }
+                }
+            }
+
+            model.put("listFavRestaurant", listFavRestaurant);
+
+        } catch (NullPointerException e) { /**/ }
+
+        /* param from setUserArea action */
+        String zoneSubmit = request.getParameter("zoneSubmit");
+        if (zoneSubmit != null) {
+            model.put("zoneSubmit", zoneSubmit);
+        }
+        /* param from nutritionPreferences action */
+        String nutritSubmit = request.getParameter("nutritSubmit");
+        if (nutritSubmit != null) {
+            model.put("nutritSubmit", nutritSubmit);
+        }
+        return new ModelAndView("edit", model);
+    }
+
+    @RequestMapping(value = {"EDIT"}, params = {"action=nutritionPreferences"})
+    public void setUserNutritionPreferences(ActionRequest request, ActionResponse response) throws Exception {
+
+        String userLogin = authenticator.getUser().getLogin();
+
+        Map parameters = request.getParameterMap();
+
+
+        int[] code = {1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15};
+
+        for (int i = 0; i < code.length; i++) {
+
+            if (parameters.get("code-" + code[i]) != null) {
+
+                try {
+                    dc.executeUpdate("INSERT INTO nutritionPreferences (USERNAME, NUTRITIONCODE) VALUES ('" + userLogin + "', '" + code[i] + "');");
+                } catch (SQLException e) { /**/ }
+
+            } else {
+                /* uncheck boxes need to be deleted from the db
+                 if it goes into the catch it mean the row didn't exist and it doesn't need specific treatment */
+                try {
+                    dc.executeUpdate("DELETE FROM nutritionPreferences WHERE USERNAME='" + userLogin + "' AND  NUTRITIONCODE='" + code[i] + "';");
+                } catch (SQLException e) { /**/ }
+
+            }
+        }
+
+        response.setRenderParameter("nutritSubmit", "true");
+    }
+
+    @RequestMapping(params = {"action=setUserArea"})
+    public void setUserArea(ActionRequest request, ActionResponse response, @RequestParam(value = "chkArea[]", required = false) String[] listAreas) throws Exception {
+
+        User user = authenticator.getUser();
+
+        String areanames = "";
+
+        if (listAreas != null) {
+
+            for (int i = 0; i < listAreas.length; i++) {
+                areanames += listAreas[i] + (i < listAreas.length - 1 ? "," : "");
+            }
+
+
+            try {
+                ResultSet results = dc.executeQuery("SELECT * FROM USERAREA WHERE USERNAME='" + user.getLogin() + "';");
+                results.next();
+                results.updateString("AREANAME", areanames);
+                results.updateRow();
+                results.close();
+            } catch (SQLException e) {
+                dc.executeUpdate("INSERT INTO USERAREA (USERNAME, AREANAME) VALUES ('" + user.getLogin() + "', '" + areanames + "');");
+            }
+
+            response.setRenderParameter("zoneSubmit", "true");
+        }
+    }
 }
